c2af3d63f6a7a14eed6939650b08174c6f535e23
Pannuba <carlomaria.99@hotmail.it>
2019-04-17 00:14:20 +0200

    main functions, sendToAll, small fixes

--- client.py
+++ client.py
@@ -23,7 +23,7 @@ def getMessage(self):
 	if message == '':		# Controlla che il messaggio non sia vuoto
 		print('Empty string, not sent')
 	
-	elif len(message) >= 512:	# Dovrei controllare dopo encoding, ma forse non cambia
+	elif len(message) > 512:	# Dovrei controllare dopo encoding, ma forse non cambia
 		print('Message is too long (< 512 chars)')
 
 	else:
@@ -34,29 +34,35 @@ def sendMessage(message):		# Finalmente funziona... Ma solo con "self". Nei tuto
 	message = message.encode('utf-8')	# Se premo solo invio il messaggio è b''
 	clientSocket.send(message)	# Se è vuoto non lo manda!
 
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+def main():
+	serverName = 'localhost'
+	serverPort = 12000
+	global clientSocket
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+	clientSocket.connect((serverName, serverPort))
 
-username = input('Username: ')
-while len(username) >= 64:
-	username = input('Username is too long (> 64 chars): ')
-username = username.encode('utf-8')
-clientSocket.send(username)
+	username = input('Username: ')
+	while len(username) > 64:
+		username = input('Username is too long (< 64 chars): ')
+	username = username.encode('utf-8')
+	clientSocket.send(username)
 
-listenThread = Thread(target=listen, args=())
-listenThread.daemon = True		# Per far chiudere il programma con quit(), altrimenti si blocca
-listenThread.start()
+	listenThread = Thread(target=listen, args=())
+	listenThread.daemon = True		# Per far chiudere il programma con quit(), altrimenti si blocca
+	listenThread.start()
 
-root = Tk()
-root.geometry('800x20')
-root.title('SuperChat 9000 - logged in as ' + username.decode('utf-8'))
-textbox = Entry(root)
-textbox.bind("<Return>", getMessage)
-textbox.pack(side = BOTTOM, fill = X)
-root.mainloop()		# Fino a che non si chiude la GUI lo script non procede
+	root = Tk()
+	root.geometry('800x20')
+	root.title('SuperChat 9000 - logged in as ' + username.decode('utf-8'))
+	global textbox
+	textbox = Entry(root)
+	textbox.bind("<Return>", getMessage)
+	textbox.pack(side = BOTTOM, fill = X)
+	root.mainloop()		# Fino a che non si chiude la GUI lo script non procede
 
-clientSocket.send('!quit'.encode('utf-8'))	# Senza questo il server non chiude il socket e va in crisi
-clientSocket.close()
-quit()
+	clientSocket.send('!quit'.encode('utf-8'))	# Senza questo il server non chiude il socket e va in crisi
+	clientSocket.close()
+	quit()
+
+if __name__ == '__main__':
+	main()
--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
+'''TODO:	* Thread per inviare comandi dal server (o accettare connessioni, uno dei due)
+			* Controllare i comandi inviati dai client (!ban, poi non saprei...)
+			* File di configurazione con la lista degli admin, IP/porta, MOTD, passsword?...
+			* Sistema di login con utente e password
+			* Mostrare i contenuti nella chat in una finestra
+'''
+
 from socket import *
 from threading import Thread
 from time import strftime
 import signal
 
-def sigint_handler(signum, frame):
+def sigint_handler(signum, frame):		# Eseguito quando viene premuto CTRL + C
 	log('\nQuitting...')
 	logfile.close()
 	for i in socketList:
@@ -17,48 +24,54 @@ def log(logMessage):	# Mostra un messaggio nel terminale e lo aggiunge a chat.lo
 	print(logMessage)
 	logfile.write(logMessage + '\n')
 
+def sendToAll(message):
+	for i in socketList:
+		i.send(message.encode('utf-8'))
+
 def handler(connectionSocket, user):
 
 		while True:
 			message = connectionSocket.recv(512)	# Attende la ricezione di un messaggio
 			message = message.decode('utf-8')	# Funzione checkmessage?
 
-			if message == '!quit':
+			if message == '!quit':		# Controllare per altri comandi (funzione?)
 				break
 
 			response = user + ": " + message
 			log(strftime('%Y-%m-%d %H:%M:%S') + ' Message from ' + response)
-
-			for i in socketList:
-				i.send(response.encode('utf-8'))
+			sendToAll(response)
 		
 		log(user + ' has left the server')		# Si termina il thread per un client connesso
-		
+		sendToAll(user + ' has left the server')
 		connectionSocket.close()				# Se non rimuovo il client disconnesso dall'array, quando
 		socketList.remove(connectionSocket)		# distribuisco il messaggio a tutti si impalla
 
-# Questo blocco viene eseguito solo all'avvio
+def main():
+	signal.signal(signal.SIGINT, sigint_handler)
 
-signal.signal(signal.SIGINT, sigint_handler)
+	serverPort = 12000
+	serverSocket = socket(AF_INET, SOCK_STREAM)
+	serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+	serverSocket.bind(('', serverPort))
+	serverSocket.listen(1)
+	global socketList
+	socketList = []
 
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
-socketList = []
+	global logfile
+	logfile = open('chat.log', 'w')
+	log(strftime('%Y-%m-%d %H:%M:%S') + ' Server booted')
 
-logfile = open('chat.log', 'a')
-log(strftime('%Y-%m-%d %H:%M:%S') + ' Server booted')
+	while True:
+		newSocket, clientAddress = serverSocket.accept()		# l'esecuzione torna all'inizio del while e si mette in "pausa" a serversocket.accept()
+		socketList.append(newSocket)
+		
+		user = newSocket.recv(64)
+		user = user.decode('utf-8')
+		log('{} has joined the server as {}'.format(clientAddress, user))	# Devo usare format() perché clientAddress è una tuple e lo manda in palla boh
+		
+		thread = Thread(target=handler, args=(newSocket, user))
+		thread.daemon = True		# Sennò quit() si blocca
+		thread.start()
 
-while True:
-	newSocket, clientAddress = serverSocket.accept()		# l'esecuzione torna all'inizio del while e si mette in "pausa" a serversocket.accept()
-	socketList.append(newSocket)
-	
-	user = newSocket.recv(64)
-	user = user.decode('utf-8')
-	log('{} has joined the server as {}'.format(clientAddress, user))	# Devo usare format() perché clientAddress è una tuple e lo manda in palla boh
-	
-	thread = Thread(target=handler, args=(newSocket, user))
-	thread.daemon = True		# Sennò quit() si blocca
-	thread.start()
+if __name__ == '__main__':
+	main()
